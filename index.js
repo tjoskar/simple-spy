2aafa63190085f09209542ec3c42c30a57ebecb6
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2017-10-26 23:08:50 +0200

    a function does not have to be provided when creating a spy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, '__esModule', {
     value: true
 });
 
-function spy(fn) {
+function spy(fn = () => undefined) {
     const stub = function (...args) {
         stub.callCount++;
         stub.args.push(args);
